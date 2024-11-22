7704fb2c79fe243674570e5075dae87c802f29bb
chefboyrd53 <143761455+chefboyrd53@users.noreply.github.com>
2024-11-22 18:42:04 -0500

    Create data.sql

---
+++ electionRepo/electionRepo/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO election (election_year, state, county, election_type, election_data, source_url)
+VALUES
+(2020, 'Colorado', 'Adams', 'General', '1703495151616', 'https://www.adamscountycoelections.gov/records-and-data-2/past-elections/#Election'),
+(2020, 'Nevada', 'Clark', 'General', '1702006869077', 'https://www.nevadacountyca.gov/3946/Cast-Vote-Record-Request-March-2024-Pres');
