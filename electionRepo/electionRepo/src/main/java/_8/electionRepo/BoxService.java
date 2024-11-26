2f09be13fd17b2c041cad61fa158052dec4141b8
chefboyrd53 <143761455+chefboyrd53@users.noreply.github.com>
2024-11-25 20:35:21 -0500

    Update BoxService.java

--- electionRepo/electionRepo/src/main/java/_8/electionRepo/BoxService.java
+++ electionRepo/electionRepo/src/main/java/_8/electionRepo/BoxService.java
@@ -2,23 +2,38 @@ package _8.electionRepo;
 
 import java.io.InputStream;
 
+import com.box.sdk.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.box.sdk.BoxAPIConnection;
-import com.box.sdk.BoxFile;
-import com.box.sdk.BoxFolder;
 
 @Service
 public class BoxService {
     private final BoxAPIConnection boxAPIConnection;
 
-    public BoxService() {
-        this.boxAPIConnection = new BoxAPIConnection("YOUR_ACCESS_TOKEN");
+    @Autowired
+    public BoxService(BoxAPIConnection boxAPIConnection) {
+        this.boxAPIConnection = boxAPIConnection;
     }
 
-    public String uploadFile(String fileName, InputStream fileStream) {
-        BoxFolder rootFolder = BoxFolder.getRootFolder(boxAPIConnection);
-        BoxFile.Info fileInfo = rootFolder.uploadFile(fileStream, fileName);
+    public String uploadFile(String fileName, InputStream fileStream, String state) {
+        BoxFolder rootFolder = new BoxFolder(boxAPIConnection, "294217161504");
+
+        BoxFolder stateFolder = rootFolder;
+        boolean stateFound = false;
+
+        for (BoxItem.Info itemInfo : rootFolder) {
+            if (itemInfo instanceof BoxFolder.Info info) {
+                if (info.getName().equals(state)) {
+                    stateFound = true;
+                    stateFolder = info.getResource();
+                }
+            }
+        }
+        if (!stateFound) {
+            stateFolder = rootFolder.createFolder(state).getResource();
+        }
+
+        BoxFile.Info fileInfo = stateFolder.uploadFile(fileStream, fileName);
         return fileInfo.getID();
     }
 }
