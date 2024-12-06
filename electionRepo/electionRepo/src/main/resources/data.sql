5955f5cb62d284b96c797abc8badddb8716e5ef6
chefboyrd53 <143761455+chefboyrd53@users.noreply.github.com>
2024-12-06 18:07:23 -0500

    Update data.sql

--- electionRepo/electionRepo/src/main/resources/data.sql
+++ electionRepo/electionRepo/src/main/resources/data.sql
@@ -1,4 +1,9 @@
 INSERT INTO election (election_year, state, county, election_type, election_data, source_url)
 VALUES
+(2024, 'California', 'El Dorado', 'Primary', '1703480332740', 'https://www.eldoradocounty.ca.gov/County-Government/Elections/Election-Results'),
+(2024, 'California', 'Nevada', 'Primary', '1703486401817', 'https://www.nevadacountyca.gov/3946/Cast-Vote-Record-Request-March-2024-Pres'),
+(2024, 'California', 'Placer', 'Primary', '1703483006322', 'https://www.placercountyelections.gov/past-elections/#03052024'),
+(2022, 'California', 'Placer', 'General', '1703497665172', 'https://www.placercountyelections.gov/past-elections/#11082022'),
 (2020, 'Colorado', 'Adams', 'General', '1703495151616', 'https://www.adamscountycoelections.gov/records-and-data-2/past-elections/#Election'),
-(2020, 'Nevada', 'Clark', 'General', '1702006869077', 'https://www.nevadacountyca.gov/3946/Cast-Vote-Record-Request-March-2024-Pres');
+(2022, 'Colorado', 'Adams', 'General', '1707626014847', 'https://www.adamscountycoelections.gov/records-and-data-2/past-elections/#Election'),
+(2020, 'Nevada', 'Clark', 'General', '1702006869077', 'https://www.clarkcountynv.gov/government/departments/elections/reports_data_maps/index.php');
